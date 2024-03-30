179df0c64901b01913498c45115ae8cd4113382c
Oscar Bashaw <oscar.bashaw@OscarB01-sigma.local>
2024-03-29 18:17:37 -0700

    add query acceleration and service type fixes to ctas

--- snowflake_cost_per_query_template/query_history_enriched.sql
+++ snowflake_cost_per_query_template/query_history_enriched.sql
@@ -165,7 +165,10 @@ with query_history as (
         usage_type,
         currency,
         effective_rate,
-        service_type
+        case 
+            when service_type = 'STORAGE' then 'STORAGE'
+            else 'COMPUTE'
+        end as service_type
     from snowflake.organization_usage.rate_sheet_daily
     where
         account_locator = current_account()
@@ -327,7 +330,8 @@ with query_history as (
             start_time
         ) as execution_start_time,
         start_time,
-        end_time
+        end_time,
+        query_acceleration_bytes_scanned
     from snowflake.account_usage.query_history
     where end_time <= (select latest_ts from stop_threshold)
     and end_time < getdate()
@@ -362,6 +366,8 @@ with query_history as (
         datediff('millisecond', greatest(execution_start_time, hour_start), least(end_time, hour_end)) as num_milliseconds_query_ran,
         sum(num_milliseconds_query_ran) over (partition by warehouse_id, hour_start) as total_query_milliseconds_in_hour,
         div0(num_milliseconds_query_ran, total_query_milliseconds_in_hour) as fraction_of_total_query_time_in_hour,
+        sum(query_acceleration_bytes_scanned) over (partition by warehouse_id, hour_start) as total_query_acceleration_bytes_scanned_in_hour,
+        div0(query_acceleration_bytes_scanned, total_query_acceleration_bytes_scanned_in_hour) as fraction_of_total_query_acceleration_bytes_scanned_in_hour,
         hour_start as hour
     from query_hours
 )
@@ -369,10 +375,14 @@ with query_history as (
 , credits_billed_hourly as (
     select
         start_time as hour,
-        warehouse_id,
-        credits_used_compute,
-        credits_used_cloud_services
+        entity_id as warehouse_id,
+        sum(iff(service_type = 'WAREHOUSE_METERING', credits_used_compute, 0)) as credits_used_compute,
+        sum(iff(service_type = 'WAREHOUSE_METERING', credits_used_cloud_services, 0)) as credits_used_cloud_services,
+        sum(iff(service_type = 'QUERY_ACCELERATION', credits_used_compute, 0)) as credits_used_query_acceleration
     from snowflake.account_usage.warehouse_metering_history
+    where true
+        and service_type in ('QUERY_ACCELERATION', 'WAREHOUSE_METERING')
+    group by 1, 2
 )
 
 , query_cost as (
@@ -380,7 +390,9 @@ with query_history as (
         query_seconds_per_hour.*,
         credits_billed_hourly.credits_used_compute * daily_rates.effective_rate as actual_warehouse_cost,
         credits_billed_hourly.credits_used_compute * query_seconds_per_hour.fraction_of_total_query_time_in_hour * daily_rates.effective_rate as allocated_compute_cost_in_hour,
-        credits_billed_hourly.credits_used_compute * query_seconds_per_hour.fraction_of_total_query_time_in_hour as allocated_compute_credits_in_hour
+        credits_billed_hourly.credits_used_compute * query_seconds_per_hour.fraction_of_total_query_time_in_hour as allocated_compute_credits_in_hour,
+        credits_billed_hourly.credits_used_query_acceleration * query_seconds_per_hour.fraction_of_total_query_acceleration_bytes_scanned_in_hour as allocated_query_acceleration_credits_in_hour,
+        allocated_query_acceleration_credits_in_hour * daily_rates.effective_rate as allocated_query_acceleration_cost_in_hour
     from query_seconds_per_hour
     inner join credits_billed_hourly
         on query_seconds_per_hour.warehouse_id = credits_billed_hourly.warehouse_id
@@ -399,6 +411,8 @@ with query_history as (
         any_value(execution_start_time) as execution_start_time,
         sum(allocated_compute_cost_in_hour) as compute_cost,
         sum(allocated_compute_credits_in_hour) as compute_credits,
+        sum(allocated_query_acceleration_cost_in_hour) as query_acceleration_cost,
+        sum(allocated_query_acceleration_credits_in_hour) as query_acceleration_credits,
         any_value(credits_used_cloud_services) as credits_used_cloud_services,
         any_value(ran_on_warehouse) as ran_on_warehouse
     from query_cost
@@ -423,6 +437,8 @@ with query_history as (
         execution_start_time,
         compute_cost,
         compute_credits,
+        query_acceleration_cost,
+        query_acceleration_credits,
         credits_used_cloud_services,
         ran_on_warehouse
     from cost_per_query
@@ -436,6 +452,8 @@ with query_history as (
         execution_start_time,
         0 as compute_cost,
         0 as compute_credits,
+        0 as query_acceleration_cost,
+        0 as query_acceleration_credits,
         credits_used_cloud_services,
         ran_on_warehouse
     from filtered_queries
@@ -451,12 +469,14 @@ with query_history as (
         all_queries.execution_start_time,
         all_queries.compute_cost,
         all_queries.compute_credits,
+        all_queries.query_acceleration_cost,
+        all_queries.query_acceleration_credits,
         -- For the most recent day, which is not yet complete, this calculation won't be perfect.
         -- So, we don't look at any queries from the most recent day t, just t-1 and before.
         (div0(all_queries.credits_used_cloud_services, credits_billed_daily.daily_credits_used_cloud_services) * credits_billed_daily.daily_billable_cloud_services) * coalesce(daily_rates.effective_rate, current_rates.effective_rate) as cloud_services_cost,
         div0(all_queries.credits_used_cloud_services, credits_billed_daily.daily_credits_used_cloud_services) * credits_billed_daily.daily_billable_cloud_services as cloud_services_credits,
-        all_queries.compute_cost + cloud_services_cost as query_cost,
-        all_queries.compute_credits + cloud_services_credits as query_credits,
+        all_queries.compute_cost + all_queries.query_acceleration_cost + cloud_services_cost as query_cost,
+        all_queries.compute_credits + all_queries.query_acceleration_credits + cloud_services_credits as query_credits,
         all_queries.ran_on_warehouse,
         coalesce(daily_rates.currency, current_rates.currency) as currency
     from all_queries
@@ -477,6 +497,8 @@ select
     cost_per_query.query_id,
     cost_per_query.compute_cost,
     cost_per_query.compute_credits,
+    cost_per_query.query_acceleration_cost,
+    cost_per_query.query_acceleration_credits,
     cost_per_query.cloud_services_cost,                                                                               
     cost_per_query.cloud_services_credits,                                                                                                                                                        
     cost_per_query.query_cost,
